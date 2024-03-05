3aecb69db45089b505f797c342961fc57a1e8c3f
Gustavo <gustavod.oliveira@outlook.com>
2024-03-04 22:15:31 -0300

    Feat: configs initials

---
+++ got/src/components/header/header.js
@@ -0,0 +1,13 @@
+import Class from './header.module.css'
+
+const Header = () => {
+    return (
+        <>
+            <header className={Class.Container}>
+                <h1> ola</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header
